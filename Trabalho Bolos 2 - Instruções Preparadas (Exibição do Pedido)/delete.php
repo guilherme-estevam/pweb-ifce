379481bb5a90687663e0a0cf720c9571d4655a27
Guilherme Estevam <101656160+guilherme-estevam@users.noreply.github.com>
2022-06-23 13:47:50 -0300

    Create delete.php

---
+++ Trabalho Bolos 2 - Instruções Preparadas (Exibição do Pedido)/delete.php
@@ -0,0 +1,24 @@
+<?php
+  include 'conn.php';
+
+  $status = "Lixo";
+  
+  $sql = "DELETE FROM bolos WHERE status = ?";
+  
+  //stmt = statment ou comando
+  //stmt é o comando a ser preparado
+  $stmt = mysqli_stmt_init($conn);  
+  $stmt_prepared_okay = mysqli_stmt_prepare($stmt, $sql);
+  // create a prepared statement
+  if($stmt_prepared_okay){
+      // Liga parametros com os marcadores
+      mysqli_stmt_bind_param($stmt, "s", $status);
+      // executa a query
+      mysqli_stmt_execute($stmt);
+      // close statement
+      mysqli_stmt_close($stmt);
+  }
+  
+  mysqli_close($conn);
+  header('location: ./index.php?filtro=Todos');
+?>
